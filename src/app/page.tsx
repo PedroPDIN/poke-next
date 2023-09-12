71e1249624eb7072c7405d52f70ec43579fa7db5
Pedro Lima <pedro.oficial1969@gmail.com>
2023-09-12 19:59:42 -0300

    feat(cardPokemon): adiciona componente cardsPokemon (estilizado).

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-import Image from "next/image";
-import Link from "next/link";
 import { dataPokemons } from "@/interfaces/IPokemon.interface";
+import CardsPokemon from "@/components/CardsPokemon";
 
 import style from "@/styles/pages/_home.module.scss"
 
@@ -10,7 +9,7 @@ async function dataPokemon(): Promise<dataPokemons> {
   const data: dataPokemons = await response.json();
 
   data.results.forEach((pokemon, index) => {
-    pokemon.id = index + 1
+    pokemon.id = index + 1;
   });
 
   return data;
@@ -22,26 +21,13 @@ export default async function Home() {
   const { results } = await dataPokemon();
 
   return (
-    <main className={ style.home_container }>
+    <main className={style.home_container}>
       {results.map((pokemon) => (
-        <article key={pokemon.id} className={ style.card_container }>
-          <div className={ style.numeric_id_pokemon }>
-            <b>{`#${pokemon.id}`}</b>
-          </div>
-
-          <div className={ style.img_pokemon}>
-            <Image
-              className={ style.img }
-              src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-              alt={pokemon.name}
-              fill
-            />
-          </div>
-
-          <p className={style.name_pokemon}>{pokemon.name}</p>
-          
-          <Link className={ style.btn_description } href={`/pokemon/${pokemon.id}`}>Descrição</Link>
-        </article>
+        <CardsPokemon
+          key={pokemon.id}
+          pokeId={pokemon.id as number}
+          name={pokemon.name}
+        />
       ))}
     </main>
   )
---
+++ src/components/CardsPokemon/index.tsx
@@ -0,0 +1,34 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import style from "@/styles/components/_cardsPokemon.module.scss";
+
+interface Props {
+  pokeId: number,
+  name: string,
+};
+
+export default function CardsPokemon({ pokeId, name }: Props) { 
+  return (
+    <section>
+      <article key={pokeId} className={style.card_container}>
+        <div className={style.numeric_id_pokemon}>
+          <b>{`#${pokeId}`}</b>
+        </div>
+
+        <div className={style.img_pokemon}>
+          <Image
+            className={style.img}
+            src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${pokeId}.svg`}
+            alt={name}
+            fill
+          />
+        </div>
+
+        <p className={style.name_pokemon}>{name}</p>
+
+        <Link className={style.btn_description} href={`/pokemon/${pokeId}`}>Descrição</Link>
+      </article>
+    </section>
+  )
+};
