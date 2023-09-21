6370c6fca72cf02260868958a25e4a31e6a28b7d
Pedro Lima <pedro.oficial1969@gmail.com>
2023-09-21 20:22:34 -0300

    refactor(error): criando componente separadamente.

--- src/app/search/[name]/error.tsx
+++ src/app/search/[name]/error.tsx
@@ -1,9 +1,7 @@
 'use client'
 
 import { useEffect } from "react"
-import Link from "next/link";
-
-import style from "../../../styles/pages/_errorSearch.module.scss";
+import ErrorFind from "@/components/ErrorFind";
 
 export default function SearchError({
   error,
@@ -17,19 +15,8 @@ export default function SearchError({
   }, [error])
   
   return (
-    <section className={style.error_search_container}>
-      <article className={style.error_search_content}>
-        <p className={style.message_error}
-        >
-          <b>Ops!!!</b> Não foi possível encontrar esse Pokémon ou houve uma falha na conexão no servidor.
-        </p>
-
-        <div className={style.solution_content}>
-          <Link className={style.btn_back} href="/">Voltar Para a Página Inicial</Link>
-          <span className={style.division_word}>OU</span>
-          <p className={style.message_solution}>Tente procurar por outro <span>Pokémon</span>.</p>
-        </div>
-      </article>
-    </section>
+    <>
+      <ErrorFind />
+    </>
   )
 };
---
+++ src/components/ErrorFind/index.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import Link from "next/link";
+import { GoAlertFill } from "react-icons/go";
+
+import style from "@/styles/pages/_errorFind.module.scss";
+
+export default function ErrorFind() {
+  return (
+    <section className={style.error_search_container}>
+      <article className={style.error_search_content}>
+        <GoAlertFill className={style.icon_error} />
+        <p className={style.message_error}
+        >
+          <b>Ops!!!</b> Não foi possível encontrar esse Pokémon ou houve uma falha na conexão no servidor.
+        </p>
+
+        <div className={style.solution_content}>
+          <Link className={style.btn_back} href="/">Voltar Para a Página Inicial</Link>
+          <span className={style.division_word}>OU</span>
+          <p className={style.message_solution}>Tente procurar por outro <span>Pokémon</span>.</p>
+        </div>
+      </article>
+    </section>
+  )
+};
