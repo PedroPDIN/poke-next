1066a5e86c52a16bda4361fa00a958d30c4f0488
Pedro Lima <pedro.oficial1969@gmail.com>
2023-09-25 16:35:28 -0300

    style: alteração minimalista a página de busca.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -7,7 +7,13 @@ export default function Header() {
   return (
     <header className={style.header_container}>
       <div className={style.header_title}>
-        <Image className={ style.icon } src="/images/pokeball.png" alt="icone da pokebola" width="42" height="42" />
+        <Image
+          className={style.icon}
+          src="/images/pokeball.png"
+          alt="icone da pokebola"
+          width="42"
+          height="42"
+        />
         <h1>Poke<span>Next</span></h1>
       </div>
       
