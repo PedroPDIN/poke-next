68596750ce02b808bab8b57526a8c59031b42d28
Pedro Lima <pedro.oficial1969@gmail.com>
2023-09-25 16:42:51 -0300

    fix: adiciona link de redirecionamento para o titulo do header.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import Link from "next/link";
 import SearchInput from "../SearchInput";
 
 import style from "@/styles/components/_header.module.scss";
@@ -6,16 +7,18 @@ import style from "@/styles/components/_header.module.scss";
 export default function Header() {
   return (
     <header className={style.header_container}>
-      <div className={style.header_title}>
-        <Image
-          className={style.icon}
-          src="/images/pokeball.png"
-          alt="icone da pokebola"
-          width="42"
-          height="42"
-        />
-        <h1>Poke<span>Next</span></h1>
-      </div>
+      <Link href="/" className={style.nav_link}>
+        <div className={style.header_title}>
+          <Image
+            className={style.icon}
+            src="/images/pokeball.png"
+            alt="icone da pokebola"
+            width="42"
+            height="42"
+          />
+          <h1>Poké<span>Next</span></h1>
+        </div>
+      </Link>
       
       <div className={style.header_search}>
         <SearchInput  />
